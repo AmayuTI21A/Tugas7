fe67dea5d90e41c781466f9ca46b6acb71232d43
AmayuTI21A <114399243+AmayuTI21A@users.noreply.github.com>
2022-11-26 20:49:38 +0700

    Add files via upload

--- Outer.java
+++ Outer.java
@@ -1,23 +1,23 @@
-package Tugas7;
 public class Outer {
      int number = 0;
 
-    private class Inner {
-        public void print() {
-            System.out.println("Mengakses inner class yang ke: " + (++number));
+    void outerMethod() {
+        class Inner{
+         public void print() {
+            System.out.println("Mengakses inner class yang ke: " + (number++));
         }
     }
-
-    void displayFromMethod() {
-        Inner in = new Inner();
-        in.print();
+        Inner inner = new Inner();
+        inner.print();
     }
 }
 
-class Main {
+class OuterAccess {
     public static void main(String[] args) {
-        Outer out = new Outer();
-        out.displayFromMethod();
-        out.displayFromMethod();
+      Outer out = new Outer ();
+      out.outerMethod();
+      out.outerMethod();
+      out.outerMethod();
+
     }
 }
